08dd54cce41432a0c30eb30e851d5770fd7b25fd
Meg Walker <meg.walker56@gmail.com>
2023-09-26 19:51:49 +0100

    initial commit of base files

---
+++ fitnessappgrp4/vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), "prettier", "eslint-plugin-prettier"],
+});
