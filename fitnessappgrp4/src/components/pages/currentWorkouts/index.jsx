7c044d8aed8401ecc122d877438072a8d1c9ac89
Saad-054 <saad.siddiqui@msn.com>
2023-09-28 21:11:04 +0100

    current workout

--- fitnessappgrp4/src/components/pages/currentWorkouts/index.jsx
+++ fitnessappgrp4/src/components/pages/currentWorkouts/index.jsx
@@ -1,13 +1,41 @@
-import App from '../../../App'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
+const CurrentWorkouts = () => {
+  // Sample data for demonstration purposes
+  const todayWorkouts = [
+    { id: 1, exercise: 'Running', duration: '30 minutes' },
+    { id: 2, exercise: 'Zumba', duration: '45 minutes' },
+  ];
 
-function currentWorkouts() {
+  const weekWorkouts = [
+    { id: 3, exercise: 'Weightlifting', duration: '60 minutes' },
+    { id: 4, exercise: 'Cycling', duration: '45 minutes' },
+  ];
 
-    return(
-        <>
-        <p>Hello</p>
-        </>
-    )
-}
+  return (
+    <div className="current-workouts">
+      <h2>Today's Workouts</h2>
+      <ul>
+        {todayWorkouts.map((workout) => (
+          <li key={workout.id}>
+            {workout.exercise} - {workout.duration}
+          </li>
+        ))}
+      </ul>
 
-export default currentWorkouts
+      <h2>This Week's Workouts</h2>
+      <ul>
+        {weekWorkouts.map((workout) => (
+          <li key={workout.id}>
+            {workout.exercise} - {workout.duration}
+          </li>
+        ))}
+      </ul>
+
+      <Link to="/future-workouts">Go to Future Workouts</Link>
+    </div>
+  );
+};
+
+export default CurrentWorkouts;
