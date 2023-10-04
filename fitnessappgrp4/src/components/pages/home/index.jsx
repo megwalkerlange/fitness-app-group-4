e19a8d0bd52d92b21eb0867347ac0744eebcb1d7
Ola Ipaye <ipayeola@outlook.com>
2023-10-04 18:14:55 +0100

    Styled homepage and centered header text.

--- fitnessappgrp4/src/components/pages/home/index.jsx
+++ fitnessappgrp4/src/components/pages/home/index.jsx
@@ -1,17 +1,14 @@
 import FitnessForm from '../../form';
-
+import css from '../home/home.css';
 function Home() {
   return (
     <>
-      <p>
-        Need help keeping track of your workouts? Do you want to set practical
-        goals that you can maintain long-term? Then ‘Fantastic Fitness Friend;
-        is the perfect companion on beginning your fitness journey. Fantastic
-        Fitness Friend allows you to look up different workouts, log your
-        progress and set yourself achievable and consistent goals. Whether
-        you’re on a sprint or a marathon Fantastic Fitness Friend will be there
-        every step of the way!
+      <header className='intro-text'>
+        <h1>Need help keeping track of your workouts?</h1>
+        <p>
+        <strong>Set practical goals. Maintain your long-term health plan.</strong> <br /> Our fitness tracker is the perfect companion to get started and succeed on your fitness journey.
       </p>
+      </header>
       <FitnessForm></FitnessForm>
     </>
   );
